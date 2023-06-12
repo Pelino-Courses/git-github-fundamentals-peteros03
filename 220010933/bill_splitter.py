4f2d0d747fc3163e62449ca7258c719231fb4b11
peteros03 <110295411+peteros03@users.noreply.github.com>
2023-06-13 00:17:27 +0200

    Update bill_splitter.py

--- 220010933/bill_splitter.py
+++ 220010933/bill_splitter.py
@@ -1,1 +1,27 @@
-
+Number_of_friends = int(input("Enter the number of friends joining (including you):\n"))
+if Number_of_friends <= 0:
+    print("No one is joining for the party")
+else:
+    Attendee = {}
+    print("Enter the name of every friend (including you), each on a new line:")
+    for names in range (Number_of_friends):
+      name=input()
+      Attendee[name]=0
+    total_bill=float(input('Enter the total bill value:\n'))
+    choices= input("Do you want to use the 'Who is lucky?'feature? Write Yes/No:\n")
+    divide_bill=round(total_bill/Number_of_friends ,2)
+    if choices.lower()=="yes":
+        import random
+        lucky=random.choice(list(Attendee.keys()))
+        print(lucky, "is the lucky one!")
+        Attendee[name]=0
+        Friends_to_pay_bills=Number_of_friends - 1
+        Second_divide_bill=round(total_bill/Friends_to_pay_bills,2)
+        for d in Attendee:
+            if d != lucky:
+                Attendee[d]= Second_divide_bill
+    else:
+        print("No one lucky to day")
+        for d in Attendee:
+            Attendee[d]=divide_bill
+    print(Attendee)
